1da0b18c2a40ca603d236bda08f7f4390963c4e0
Jose Toquica <jose.herley.2010@gmail.com>
2022-06-23 19:27:41 -0500

    Validación parametros

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body, Query
 
 app = FastAPI()
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get('/')
 def home():
     return {"Hello": "World"}
+
+@app.post('/person/new')
+def createPerson(person: Person = Body(...)):
+    return person
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: int = Query(...)
+):
+    return {"age": age}
+
