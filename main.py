59c13b8df9f88b1e8ea575aa9695884a320e0128
Jose Toquica <jose.herley.2010@gmail.com>
2022-06-23 20:31:56 -0500

    Operación Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -22,8 +22,31 @@ def createPerson(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+    ),
     age: int = Query(...)
 ):
     return {"age": age}
 
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(..., gt=0)
+):
+    return {"id": person_id}
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...)
+):
+    return person
